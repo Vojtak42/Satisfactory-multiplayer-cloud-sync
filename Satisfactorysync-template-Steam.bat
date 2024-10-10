5ce6b048bc8f04d2d0282d0208e1da8989c31a26
Vojtak42 <Vojtak42@email.cz>
2024-10-10 16:31:25 +0200

    initial upload

---
+++ Satisfactorysync-template-Steam.bat
@@ -0,0 +1,30 @@
+@echo off
+set "id=[ID]"
+echo Checking if someone else isn't online
+rclone ls satisfactory:online >nul  2>&1
+IF ERRORLEVEL 1 (GOTO noone) ELSE (GOTO online)
+:online
+echo Someone else is already online, join their session
+pause
+exit
+:noone
+rclone mkdir satisfactory:online
+echo Downloading saves
+rename %localappdata%\FactoryGame\Saved\SaveGames\%id% Single
+rclone sync satisfactory:MP %localappdata%\FactoryGame\Saved\SaveGames\MP
+rename %localappdata%\FactoryGame\Saved\SaveGames\MP %id%
+echo Starting the game
+start /W "" "steam://rungameid/526870"
+timeout /t 20 /nobreak
+:loop
+timeout /T 10 /nobreak  >nul
+tasklist | find  /i "FactoryGameSteam.exe" >nul  2>&1
+IF ERRORLEVEL 1 (GOTO continue) ELSE (GOTO loop)
+:continue
+echo Waiting 30 seconds for cloud saving
+TIMEOUT /T 30
+rename %localappdata%\FactoryGame\Saved\SaveGames\%id% MP
+echo Uploading saves
+rclone sync %localappdata%\FactoryGame\Saved\SaveGames\MP satisfactory:MP
+rclone rmdir satisfactory:online
+rename %localappdata%\FactoryGame\Saved\SaveGames\Single %id%
